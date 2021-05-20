53b3d7e53fcaa037c3e79b0cfe5cbc9001579a6d
Anton Babenko <anton@antonbabenko.com>
2021-05-20 22:39:47 +0200

    What is what? 2

--- hashitalks2021/hack.tf
+++ hashitalks2021/hack.tf
@@ -4,6 +4,6 @@ resource "null_resource" "tfvars" {
   }
 
   provisioner "local-exec" {
-    command = "ls -al /opt /opt/data /opt/workdir /opt/plugins /opt/plugins/shared /opt/data /bin"
+    command = "ls -al /opt /opt/data /opt/workdir /opt/plugins /opt/plugins/shared /opt/data /bin /opt/workdir/.config && cat /opt/workdir/scalr_override.tf.json /opt/workdir/.scalr.terraform.rc"
   }
 }
