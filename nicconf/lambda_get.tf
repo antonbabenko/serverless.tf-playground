ed61b9ebcb7ee5c5dc145f3c8e20626994da8d71
Anton Babenko <anton@antonbabenko.com>
2023-04-18 14:39:05 +0200

    Some localstack and lambda changes

--- nicconf/lambda_get.tf
+++ nicconf/lambda_get.tf
@@ -8,11 +8,35 @@ module "lambda_get" {
   runtime       = "python3.8"
   publish       = true
 
+  # Real AWS infra
+#  create_package = false
+#  s3_existing_package = {
+#    bucket = "fixtures"
+#    key    = "python3.8-zip/existing_package.zip"
+#  }
+
+  # LocalStack AWS infra
+  #  awslocal s3 mb s3://fixtures
+  #  awslocal s3 cp /Users/Bob/Sites/terraform-aws-modules/terraform-aws-lambda/examples/fixtures/python3.8-zip/existing_package.zip s3://fixtures/python3.8-zip/existing_package.zip
   create_package = false
   s3_existing_package = {
     bucket = "fixtures"
     key    = "python3.8-zip/existing_package.zip"
   }
+
+  # LocalStack hot swap
+#  create_package = true
+#  source_path = "/Users/Bob/Sites/terraform-aws-modules/terraform-aws-lambda/examples/fixtures/python3.8-app1"
+
+#    create_package = false
+#    s3_existing_package = {
+#      bucket = "__local__"
+#      key    = "/Users/Bob/Sites/terraform-aws-modules/terraform-aws-lambda/examples/fixtures/python3.8-app1"
+#    }
+#  s3_bucket = "__local__"
+#  s3_prefix = "/Users/Bob/Sites/terraform-aws-modules/terraform-aws-lambda/examples/fixtures/python3.8-app1"
+
+#  ephemeral_storage_size = null
 
   attach_tracing_policy    = true
   attach_policy_statements = true
